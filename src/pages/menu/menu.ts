bf77d1b4a3e15c271c0ff081955d49fe9fd6631a
ANDRES BRAVO <andres_f5@hotmail.com>
2017-10-07 18:04:59 -0500

    tabs y menu funcionando ready

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { LoginPage } from '../login/login';
-
-/**
- * Generated class for the MenuPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { VenderPage } from'../vender/vender';
+import { ArrendarPage }from'../arrendar/arrendar';
+import{ ComprarPage }from'../comprar/comprar';
+import{ BuscarPage }from'../buscar/buscar';
+import { HomePage } from '../home/home';
+import { MapaPage } from '../mapa/mapa';
 
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
 export class MenuPage {
+
+  tab1Root = BuscarPage;
+  tab2Root = VenderPage;
+  tab3Root = ComprarPage;
+  tab4Root = ArrendarPage;
+  tab5Root = MapaPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -23,3 +28,11 @@ export class MenuPage {
   }
 
 }
+
+// -------------- desde  aqui va el nuevo codigo
+
+
+
+
+
+
